092d03e52ad4f0b858f539b89bbe0f7e0846e2c4
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2023-03-11 17:22:13 -0600

    Automatically detect Steam on Linux and Mac

--- common.py
+++ common.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 
@@ -7,10 +8,22 @@ if len(sys.argv) == 1:
 
 portal_2 = sys.argv[1] == 'want_you_gone'
 
-steam_root = (
-    sys.argv[2]
-    if len(sys.argv) > 2
-    else 'C:\\Program Files (x86)\\Steam\\steamapps\\common'
-)
-portal_root = f'{steam_root}/Portal{" 2" * portal_2}/portal{"2" * portal_2}'
+if len(sys.argv) > 2:
+    steam_common = sys.argv[2]
+else:
+    for steam_dir in [
+        'C:\\Program Files (x86)\\Steam',
+        'C:\\Program Files\\Steam',
+        os.path.expanduser('~/.local/share/Steam'),
+        os.path.expanduser('~/.var/app/com.valvesoftware.Steam/data/Steam'),
+        os.path.expanduser('~/Library/Application Support/Steam'),
+    ]:
+        if os.path.isdir(steam_dir):
+            steam_common = f'{steam_dir}/steamapps/common'
+            break
+    else:
+        print("The location of Steam couldn't be determined automatically. Please specify the location manually.")
+        sys.exit(1)
+
+portal_root = f'{steam_common}/Portal{" 2" * portal_2}/portal{"2" * portal_2}'
 vpk_path = f'{portal_root}/{"pak01" if portal_2 else "portal_pak"}_dir.vpk'
