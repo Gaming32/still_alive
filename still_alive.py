0f1e44bc4729efe6207da650c31e10cab0d7c37d
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2023-03-11 12:44:29 -0600

    Revert threading

--- still_alive.py
+++ still_alive.py
@@ -1,8 +1,6 @@
-import threading
 import os
 import sys
 import time
-from typing import Optional
 
 import playsound
 import vpk
@@ -15,22 +13,15 @@ with vpk.open(vpk_path) as vf:
     with vf[f'sound/music/{song_name}'] as fp_in:
         fp_in.save(song_name)
 
-
-song_thread: Optional[threading.Thread] = None
-
-def start_song() -> None:
-    global song_thread
-    if song_thread is None:
-        song_thread = threading.Thread(target=playsound.playsound, args=(song_name, True), daemon=True)
-        song_thread.start()
-
+played_sound = False
 
 target_time = time.perf_counter()
 clear_command = 'cls' if sys.platform == 'win32' else 'clear'
 os.system(clear_command)
 for (delay, char) in still_alive_data_reader.read_chars():
-    if portal_2 and char == '2' and song_thread is None:
-        start_song()
+    if portal_2 and char == '2' and not played_sound:
+        played_sound = True
+        playsound.playsound(song_name, block=False)
     if char == '\0':
         os.system(clear_command)
     else:
@@ -39,10 +30,10 @@ for (delay, char) in still_alive_data_reader.read_chars():
     target_time += delay
     while time.perf_counter() < target_time:
         pass
-    if not portal_2 and delay > 2.5 and song_thread is None:
+    if not portal_2 and delay > 2.5 and not played_sound:
         played_sound = True
-        start_song()
+        playsound.playsound(song_name, block=False)
 
 print()
-if song_thread is not None:
-    song_thread.join()
+if portal_2:
+    time.sleep(10) # Portal 2's credits.txt ends before the song
